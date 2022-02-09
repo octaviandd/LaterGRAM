2a4f9367ecdf3fec191bbb66535f39a3e5d84d53
Octavian David <45077244+octaviandd@users.noreply.github.com>
2022-02-09 15:53:22 +0000

    new link

--- client/index.js
+++ client/index.js
@@ -14,16 +14,8 @@ import { createUploadLink } from "apollo-upload-client";
 import { setContext } from "@apollo/client/link/context";
 import Theme from "./Theme.jsx";
 
-let uri = "";
-
-if (process.env.NODE_ENV === "production") {
-  uri = "https://laterrgram.herokuapp.com/graphql";
-} else {
-  uri = "http://localhost:4000/graphql";
-}
-
 const link = createUploadLink({
-  uri: "http://localhost:4000/graphql",
+  uri: "https://laterrgram.herokuapp.com/graphql",
 });
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem("token");
